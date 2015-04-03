78c647d11f485fa1173e64ef8da60c65c0aa8499
Werner Froidevaux <wfro@users.sourceforge.net>
2015-04-03 14:04:02 +0200

    Clean-up phase 6

--- src/java/org/opentdc/users/file/FileServiceProvider.java
+++ src/java/org/opentdc/users/file/FileServiceProvider.java
@@ -23,6 +23,139 @@
  */
 package org.opentdc.users.file;
 
-public class FileServiceProvider {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.logging.Logger;
 
+import javax.servlet.ServletContext;
+
+import org.opentdc.service.exception.DuplicateException;
+import org.opentdc.service.exception.NotFoundException;
+import org.opentdc.users.ServiceProvider;
+import org.opentdc.users.UserModel;
+
+public class FileServiceProvider implements ServiceProvider {
+
+	private static Map<String, UserModel> data = new HashMap<String, UserModel>();
+	private static final Logger logger = Logger.getLogger(FileServiceProvider.class.getName());
+
+	public FileServiceProvider(
+		ServletContext context, 
+		String prefix
+	) {
+		logger.info("> FileServiceProvider()");
+	}
+	
+	private void setNewID(UserModel dataObj) {
+		String _id = UUID.randomUUID().toString();
+		dataObj.setId(_id);
+	}
+
+	private void storeData(UserModel dataObj) {
+		data.put(dataObj.getId(), dataObj);
+	}
+
+	private UserModel getData(String id) {
+		return data.get(id);
+	}
+
+	private List<UserModel> getData() {
+		return new ArrayList<UserModel>(data.values());
+	}
+
+	private int getDataSize() {
+		int _retVal = 0;
+		if (data != null) {
+			_retVal = data.size();
+		}
+		return _retVal;
+	}
+
+	private void removeData(String id) {
+		data.remove(id);
+	}
+
+	private void clearData() {
+		data.clear();
+	}
+	
+	@Override
+	public List<UserModel> list(
+		String queryType,
+		String query,
+		long position,
+		long size
+	) {
+		List<UserModel> _list = getData();
+		logger.info("list() -> " + getDataSize() + " values");
+		return _list;
+	}
+
+	@Override
+	public UserModel create(
+		UserModel user
+	) throws DuplicateException {
+		if (getData(user.getId()) != null) {
+			throw new DuplicateException();
+		}
+		setNewID(user);
+		storeData(user);
+		return user;
+	}
+
+	@Override
+	public UserModel read(
+		String id
+	) throws NotFoundException {
+		UserModel _dataObj = getData(id);
+		if (_dataObj == null) {
+			throw new NotFoundException();
+		}
+		// response.setId(id);
+		logger.info("read(" + id + "): " + _dataObj);
+		return _dataObj;
+	}
+
+	@Override
+	public UserModel update(
+		String id,
+		UserModel user
+	) throws NotFoundException {
+		if (getData(id) == null) {
+			throw new NotFoundException();
+		} else {
+			user.setId(id);
+			storeData(user);
+			return user;
+		}
+	}
+
+	@Override
+	public void delete(
+		String id
+	) throws NotFoundException {
+		UserModel user = getData(id);
+		if (user == null) {
+			throw new NotFoundException();
+		}
+		removeData(id);
+		logger.info("delete(" + id + ")");		
+	}
+
+	@Override
+	public void deleteAll(
+	) {
+		clearData();
+		logger.info("all data deleted");		
+	}
+
+	@Override
+	public int count(
+	) {
+		return getDataSize();
+	}
+	
 }
