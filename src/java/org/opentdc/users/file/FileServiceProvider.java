d3f16c2216899915ea954d35137a792b12a1ec4a
Werner Froidevaux <wfro@users.sourceforge.net>
2015-04-03 17:56:11 +0200

    Clean-up phase 7

--- src/java/org/opentdc/users/file/FileServiceProvider.java
+++ src/java/org/opentdc/users/file/FileServiceProvider.java
@@ -46,7 +46,6 @@ public class FileServiceProvider implements ServiceProvider {
 		ServletContext context, 
 		String prefix
 	) {
-		logger.info("> FileServiceProvider()");
 	}
 	
 	private void setNewID(UserModel dataObj) {
